f4041fadf77be4cbf60f77478ebee192ee270f91
burrezo <burrezo@gmail.com>
2022-04-17 15:51:48 +0200

    findTheOldest done!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayObj) {
 
+    arrayObj.forEach(person => person.yearOfDeath ||= (new Date).getFullYear());
+
+    const arrayObjSort = arrayObj.sort((previous, current) => (current.yearOfDeath - current.yearOfBirth) - (previous.yearOfDeath - previous.yearOfBirth));
+
+    return arrayObjSort[0];
+    
 };
 
 // Do not edit below this line
