97bdcc619e026a7dd73d87da54b8c85d7b73b429
burrezo <burrezo@gmail.com>
2022-04-17 12:20:50 +0200

    palindrome passed!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (palind) {
+    let palindLowerc = palind.toLowerCase().split('').filter(char => char.match(/[a-z]/));
+    let palindLowercRev = [...palindLowerc];
+    palindLowercRev.reverse();
 
+    // console.table(palindLowerc);
+    // console.table(palindLowercRev);
+
+    for(let i = 0; i < palindLowerc.length; i++){
+        // console.log(palindLowerc[i], palindLowercRev[i]);
+        if(palindLowerc[i] !== palindLowercRev[i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
