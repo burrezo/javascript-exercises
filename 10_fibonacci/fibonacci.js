d0f02dfee68100f344310e92f2b2fe6fe13bf9a3
burrezo <burrezo@gmail.com>
2022-04-17 13:18:21 +0200

    fibonacci passed!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    const fibo = [1,1];
 
+    if (member < 0) return 'OOPS';
+
+    else if (member > 2){
+        for(let i=2; i <= (member-1); i++){
+            fibo.push(fibo[i-2]+fibo[i-1]);
+        }
+    }
+//    console.table(fibo); 
+    return fibo[member-1];
 };
+
+// console.log(fibonacci(25));
 
 // Do not edit below this line
 module.exports = fibonacci;
