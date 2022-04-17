8afe10ff8058726a49b2faccae0130888e52302e
burrezo <burrezo@gmail.com>
2022-04-17 11:46:43 +0200

    calculator finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a = 0, b = 0) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a = 0, b = 0) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((sum, e) => sum + e, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((current, previous) => current * previous);
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+  return Math.pow(base,pow);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) return 1;
+  let total = 1;
+  for(let i = num; i > 0; i--){
+    total *= i;
+  }
+  return total;
+  
 };
 
 // Do not edit below this line
